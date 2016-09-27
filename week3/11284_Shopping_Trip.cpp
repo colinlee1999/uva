04401581bd8fa9b0cc8795981198fd1de8a6f6f9
colinlee1999 <arteest@foxmail.com>
2016-09-26 22:50:26 -0500

    11284 debugging

--- week3/11284_Shopping_Trip.cpp
+++ week3/11284_Shopping_Trip.cpp
@@ -11,38 +11,40 @@
 #define INT_MAX (~INT_MIN)
 #define UNREACHABLE (INT_MAX>>2)
 #define INF (1e300)
+#define eps (1e-9)
 using namespace std;
 
 ifstream fin("11284_input.txt");
-#define cin fin
+//#define cin fin
 
 struct dvd_info
 {
-	dvd_info(int id_store, double gain)
+	dvd_info(int id_store, long long gain)
 	{
 		this->id_store = id_store;
 		this->gain = gain;
 	}
 
 	int id_store;
-	double gain;
+	long long gain;
 };
 
 int main()
 {
 	int tttt;
-	cin >> tttt;
+	scanf("%d", &tttt);
 	while (tttt--)
 	{
 		int n, m;
-		cin >> n >> m;
-		vector<vector<double> > dist(n + 1, vector<double>(n + 1, INF));
+		scanf("%d %d", &n, &m);
+		vector<vector<long long> > dist(n + 1, vector<long long>(n + 1, INT_MAX));
 		for (int i = 0; i < m; i++)
 		{
 			int s, t;
-			double d;
-			cin >> s >> t >> d;
-			dist[s][t] = dist[t][s] = d;
+			long long d1, d2;
+			scanf("%d %d %lld.%lld", &s, &t, &d1, &d2);
+			// cout << "debug:\t" << d1 << d2 << endl;
+			dist[s][t] = dist[t][s] = d1 * 100 + d2;
 		}
 		
 		for (int i = 0; i <= n; i++)
@@ -54,37 +56,51 @@ int main()
 					dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
 
 		int p;
-		cin >> p;
-		vector<dvd_info> dvd(p + 1, dvd_info(0, 0));
-		for (int i = 1; i <= p; i++)
-			cin >> dvd[i].id_store >> dvd[i].gain;
+		scanf("%d", &p);
+		vector<dvd_info> dvd(p, dvd_info(0, 0));
+		for (int i = 0; i < p; i++)
+		{
+			long long d1, d2;
+			scanf("%d %lld.%lld", &dvd[i].id_store, &d1, &d2);
+			dvd[i].gain = d1 * 100 + d2;
+		}
 
-		vector<vector<double> > f(p + 1, vector<double>((1 << (p + 1)), -INF));
+		vector<vector<long long> > f(p, vector<long long>((1 << (p)), INT_MIN));
+
+		for (int i = 0; i < p; i++)
+			f[i][1 << i] = - dist[dvd[i].id_store][0] + dvd[i].gain;
+
+		vector< pair<int, int> > o;
+		for (int i = 0; i < (1 << (p)); i++) {
+			o.push_back(make_pair(__builtin_popcount(i), i));
+		}
+		sort(o.begin(), o.end());
 		
-		f[0][1] = 0;
-
-		for (int j = 3; j < (1 << (p + 1)); j++)
-			for (int i = 1; i <= p; i++)
-				for (int k = 0; k <= p; k++)
-					if (i != k)
+		for (int j = 3; j < (1 << (p)); j++)
+			for (int i = 0; i < p; i++) // to position i
+			{
+				int to = 1 << i;
+				for (int k = 0; k < p; k++) // from position k
+					if (i != k && f[k][j & (~to)] != INT_MIN)
 					{
-						int to = 1 << i;
 						int from = 1 << k;
-						if ((to & j) && (from & j))
+						if ((to & j) && (from & j)) // j contains to and from
 						{
 							f[i][j] = max(f[i][j], f[k][j & (~to)] + dvd[i].gain - dist[dvd[k].id_store][dvd[i].id_store]);
 							//f[i][j] = max(f[i][j], f[k][j] + dvd[i].gain - dist[dvd[k].id_store][dvd[i].id_store]);
 						}
 					}
+			}
 
-		double result = -INF;
-		for (int j = 1; j < (1 << (p + 1)); j++)
-			for (int i = 1; i <= p; i++)
-				result = max(result, f[i][j] - dist[dvd[i].id_store][0]);
+		long long result = 0;
+		for (int j = 0; j < (1 << (p)); j++)
+			for (int i = 0; i < p; i++)
+				if (f[i][j] != INT_MIN)
+					result = max(result, f[i][j] - dist[dvd[i].id_store][0]);
 
 		if (result <= 0)
-			cout << "Don't leave the house" << endl;
+			printf("Don't leave the house\n");
 		else
-			printf("Daniel can save $%.2f\n", result);
+			printf("Daniel can save $%lld.%02lld\n", result / 100, result % 100);
 	}
 }
